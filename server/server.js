939884982076ba02f7bc081791838c4c1af52fb8
YashuAggarwall <yashuaggarwal141@gmail.com>
2026-02-11 14:00:51 +0530

    Fixed Cors in server.js

--- server/server.js
+++ server/server.js
@@ -29,18 +29,10 @@ const allowedOrigins = [
 ].filter(Boolean);
 
 app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true,
-    optionsSuccessStatus: 200
+  origin: true,
+  credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
